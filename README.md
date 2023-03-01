00930bc4d16194665f1e964ebb981d8f2c0db8e0
Igor Pavlov <bujhm9987@gmail.com>
2023-03-01 13:34:30 +0300

    update project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/bujhm9987/java-project-61/workflows/hexlet-check/badge.svg)](https://github.com/bujhm9987/java-project-61/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/c798c0b5338683ec6916/maintainability)](https://codeclimate.com/github/bujhm9987/java-project-61/maintainability)
+
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,28 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
+        int userChoice = Integer.parseInt(scanner.next());
+
+        switch (userChoice) {
+            case (0):
+                break;
+            case (1):
+                Cli.greet();
+                break;
+            default:
+                System.out.println("You entered an invalid game number");
+                break;
+        }
+        scanner.close();
     }
 
 }
