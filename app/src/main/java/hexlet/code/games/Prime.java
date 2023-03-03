7dcb73dfdb60bc34b50fd750a65185f0b9685fa6
Igor Pavlov <bujhm9987@gmail.com>
2023-03-03 23:50:21 +0300

    update Prime

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -7,12 +7,12 @@ import java.util.Random;
 public class Prime {
     public static void main() {
         Engine.welcome();
-        int randomBound = 100;
+        int randomBound = 99;
 
         System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
 
         while (Engine.countGame < 3) {
-            int randomNumber = new Random().nextInt(randomBound);
+            int randomNumber = new Random().nextInt(randomBound) + 1;
             int i = 2;
             int j = 0;
             while (i * i <= randomNumber && j != 1) {
